661aa39cad3f03828c4877bf5355656bd8a245d6
Gonzales-9E-ay2526 <gonzales.jm@obmontessori.edu.ph>
2026-02-20 11:29:48 +0800

    Add files via upload

---
+++ mai.js
@@ -0,0 +1,15 @@
+// Function for Discount Button
+function buttonDiscount (){
+    let CustomerBudget = document.getElementById(budget).value;
+    const discount = .2;
+    let discountedPrice = Number(CustomerBudget * 0.2); 
+
+    document.getElementById().innerHTML = '
+        <div class="container">
+
+
+        </div>
+    
+    
+    '
+}
