e36f895fee3b09ef3b90775f356981d12adeead0
Gonzales-9E-ay2526 <gonzales.jm@obmontessori.edu.ph>
2026-02-27 10:32:18 +0800

    Update mai.js

--- mai.js
+++ mai.js
@@ -1,15 +1,37 @@
 // Function for Discount Button
-function buttonDiscount (){
-    let CustomerBudget = document.getElementById(budget).value;
-    const discount = .2;
+function buttonDiscount(){
+    let CustomerBudget = document.getElementById("budget").value;
     let discountedPrice = Number(CustomerBudget * 0.2); 
 
-    document.getElementById().innerHTML = '
-        <div class="container">
+    document.getElementById("disc-box").innerHTML =`
+        <div class="container bg-secondary  bg-white rounded mt-5">
+                <h4 class="text-white">Recommended Brands:</h4>
 
+                <button class="btn btn-primary mt-3">Acer</button>
+                <button class="btn btn-primary mt-3">Lenovo</button>
+                <button class="btn btn-primary mt-3">Asus</button>
 
-        </div>
-    
-    
-    '
+                <p class="text-muted">Elligible discount: <span class="bg-danger">${discountedPrice}</span></p>
+
+        </div>`;        
+}
+
+function storelocations(){
+    document.getElementById("loc-box").innerHTML =`
+        <div class="container bg-secondary bg-white rounded mt-5">
+                <h4 class="text-white">Store branches:</h4>
+
+                <h5 class="text-warning mt-3>Acer</h5> 
+                <p class="text-warning"> ACER Concept Store Greenhills</p>
+                <p class="text-warning"> ACER Flagship Store SM Megamall</p>
+
+                <h5 class="text-warning mt-3>Lenovo</h5> 
+                <p class="text-warning"> Lenovo @ LES - MegaMall</p>
+                <p class="text-warning"> Lenovo Service Center - SM Megamall</p>
+
+                <h5 class="text-warning mt-3>Asus</h5> 
+                <p class="text-warning"> ASUS Concept Store Greenhills Mall</p>
+                <p class="text-warning"> ASUS Concept Store SM Megamall</p>
+       
+        </div>`;  
 }
